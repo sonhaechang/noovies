5a2cefe4028a594fae5f4316611f6b4eac4e7827
Son Haechang <sonhaechang@Sonui-MacBookPro.local>
2023-05-18 01:06:36 +0900

    refector: SplashScreen part Three
    1. loadFonts, loadImages 유틸리티 함수를 만들어서 여러 이미지나 폰트 불러오기
    2. useAssets, useFonts hook을 사용해서 여러 이미지와 폰트 불러오기

--- App.js
+++ App.js
@@ -1,43 +1,58 @@
 import React, { useCallback, useEffect, useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Image, Text, View } from 'react-native';
 
 import { Ionicons } from '@expo/vector-icons'
+import { Asset, useAssets } from 'expo-asset';
 import * as Font from 'expo-font';
 import * as SplashScreen from 'expo-splash-screen';
 
 SplashScreen.preventAutoHideAsync();
 
+// const loadFonts = (fonts) => fonts.map((font) => Font.loadAsync(font));
+
+// const loadImages = (images) => images.map((image) => {
+// 	if (typeof image === 'string') {
+// 		return Image.prefetch(image);
+// 	} else {
+// 		return Asset.loadAsync(image);
+// 	}
+// })
+
 export default function App() {
-	const [ready, setReady] = useState(false);
-
-	useEffect(() => {
-		async function prepare() {
-			try {
-
-        		await Font.loadAsync(Ionicons.font);
-				// import { Asset } from 'expo-asset';
-				// await Asset.loadAsync(require(url)); // In local file system 
-
-				// inport { Image } from 'react-native'
-				// await Image.prefetch(url); // on the Web
-				setReady(true);
-			} catch (e) {
-				console.log(e);
-			} 
-		}
+	// const [ready, setReady] = useState(false);
 	
-		prepare();
-	}, []);
+	// useEffect(() => {
+	// 	async function prepare() {
+	// 		try {
+	// 			const fonts = loadFonts([Ionicons.font]);
+	// 			const images = loadImages([require('./bus.jpg')]);
+
+	// 			await Promise.all([...fonts, images]);
+	// 		} catch (e) {
+	// 			console.warn(e);
+	// 		} finally {
+	// 			setReady(true);
+	// 		}
+	// 	}
+
+	// 	prepare();
+	// }, []);
+	
+	// const onLayoutRootView = useCallback(async () => {
+	// 	if (ready) { await SplashScreen.hideAsync(); }
+	// }, [ready]);
+	
+	// if (!ready) { return null; }
+
+	// // only use image and font
+	const [asset] = useAssets([require('./bus.jpg')]);
+	const [loaded] = Font.useFonts(Ionicons.font)
 
 	const onLayoutRootView = useCallback(async () => {
-		if (ready) {
-			await SplashScreen.hideAsync();
-		}
-	}, [ready]);
+		if (asset || loaded) { await SplashScreen.hideAsync(); }
+	}, [asset, loaded]);
 
-	if (!ready) {
-		return null;
-	}
+	if (!asset || !loaded) { return null; }
 
 	return (
 		<View onLayout={onLayoutRootView}>
