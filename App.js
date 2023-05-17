5c876026e97ad6b59ddd3f1a444ebbbc8a80641e
Son Haechang <sonhaechang@Sonui-MacBookPro.local>
2023-05-18 03:24:20 +0900

    feat: Create Navigation
    1. install '@react-navigation/native 및 의존성 라이브러리 설치
    2. install @react-navigation/bottom-tabs
    3. Create navigation/Tabs.js
    4. Create screens/Movies.js, Search.js, Tv.js
    5. App.js에 NavigationContainer와 Tabs 추가

--- App.js
+++ App.js
@@ -1,62 +1,25 @@
-import React, { useCallback, useEffect, useState } from 'react';
-import { StyleSheet, Image, Text, View } from 'react-native';
+import React, { useCallback } from 'react';
 
-import { Ionicons } from '@expo/vector-icons'
-import { Asset, useAssets } from 'expo-asset';
 import * as Font from 'expo-font';
 import * as SplashScreen from 'expo-splash-screen';
+import { Ionicons } from '@expo/vector-icons'
 
-SplashScreen.preventAutoHideAsync();
+import { NavigationContainer } from '@react-navigation/native';
 
-// const loadFonts = (fonts) => fonts.map((font) => Font.loadAsync(font));
-
-// const loadImages = (images) => images.map((image) => {
-// 	if (typeof image === 'string') {
-// 		return Image.prefetch(image);
-// 	} else {
-// 		return Asset.loadAsync(image);
-// 	}
-// })
+import Tabs from './navigation/Tabs';
 
 export default function App() {
-	// const [ready, setReady] = useState(false);
-	
-	// useEffect(() => {
-	// 	async function prepare() {
-	// 		try {
-	// 			const fonts = loadFonts([Ionicons.font]);
-	// 			const images = loadImages([require('./bus.jpg')]);
-
-	// 			await Promise.all([...fonts, images]);
-	// 		} catch (e) {
-	// 			console.warn(e);
-	// 		} finally {
-	// 			setReady(true);
-	// 		}
-	// 	}
-
-	// 	prepare();
-	// }, []);
-	
-	// const onLayoutRootView = useCallback(async () => {
-	// 	if (ready) { await SplashScreen.hideAsync(); }
-	// }, [ready]);
-	
-	// if (!ready) { return null; }
-
-	// // only use image and font
-	const [asset] = useAssets([require('./bus.jpg')]);
 	const [loaded] = Font.useFonts(Ionicons.font)
 
 	const onLayoutRootView = useCallback(async () => {
-		if (asset || loaded) { await SplashScreen.hideAsync(); }
-	}, [asset, loaded]);
+		if (loaded) { await SplashScreen.hideAsync(); }
+	}, [loaded]);
 
-	if (!asset || !loaded) { return null; }
+	if (!loaded) { return null; }
 
 	return (
-		<View onLayout={onLayoutRootView}>
-		  	<Text>Open up App.js to start working on your app!</Text>
-		</View>
+		<NavigationContainer onLayout={onLayoutRootView}>
+			<Tabs />
+		</NavigationContainer>
 	  );
 }
---
+++ screens/Tv.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default function Tv() {
+    return (
+        <View 
+            style={{ 
+                flex: 1, 
+                justifyContent: 'center',
+                alignItems: 'center',
+            }}
+        >
+            <Text>Tv</Text>
+        </View>
+    );
+}
