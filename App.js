3f5f547f121b61160fc8bed1a8eb8b9d4abb1627
Son Haechang <sonhaechang@Sonui-MacBookPro.local>
2023-05-18 03:57:09 +0900

    refector: Configuring the Navigation
    1. createBottomTabNavigatordml screenOptions options 적용해서 디자인 수정하는 방법
    2. initialRouteName로 시작 페이지 설정
    3. screenOptions의 tabBarStyle, tabBarActiveTintColor 적용

--- App.js
+++ App.js
@@ -21,5 +21,5 @@ export default function App() {
 		<NavigationContainer onLayout={onLayoutRootView}>
 			<Tabs />
 		</NavigationContainer>
-	  );
+	);
 }
--- navigation/Tabs.js
+++ navigation/Tabs.js
@@ -9,7 +9,13 @@ const Tab = createBottomTabNavigator();
 
 export default function Tabs() {
     return (
-        <Tab.Navigator>
+        <Tab.Navigator 
+            initialRouteName='Movies'
+            screenOptions={{ 
+                tabBarStyle: { paddingTop: 10 },
+                tabBarActiveTintColor: '#4F46E5',
+            }}
+        >
             <Tab.Screen name='Movies' component={Movies} />
             <Tab.Screen name='Tv' component={Tv} />
             <Tab.Screen name='Search' component={Search} />
