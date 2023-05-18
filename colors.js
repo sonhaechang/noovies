05829b3fd862f79a3f8816586a27044d0e7240c0
Son Haechang <sonhaechang@Sonui-MacBookPro.local>
2023-05-18 14:16:11 +0900

    feat: Create Dark Mode
    1. useColorScheme Hooks를 사용해서 현재 모드 감지
    2. colors.js 생성해서 자주 사용하는 색상 모음 객체 생성
    3. useColorScheme과 colors objects를 Navigator에 다크 모드 적용

---
+++ colors.js
@@ -0,0 +1,6 @@
+export const colors = {
+    dark: '#1e272e',
+    darkGrey: '#d3dae2',
+    lightGrey: '#808e9b',
+    yellow: '#ffa801',
+}
--- navigation/Tabs.js
+++ navigation/Tabs.js
@@ -1,19 +1,29 @@
 import React from 'react';
+import { useColorScheme } from 'react-native';
 
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Movies from '../screens/Movies';
 import Tv from '../screens/Tv';
 import Search from '../screens/Search';
+import { colors } from '../colors';
 
 const Tab = createBottomTabNavigator();
 
 export default function Tabs() {
+    const isDark = useColorScheme() == 'dark';
+
     return (
         <Tab.Navigator 
             initialRouteName='Movies'
             screenOptions={{ 
-                tabBarStyle: { paddingTop: 10 },
-                tabBarActiveTintColor: '#4F46E5',
+                tabBarStyle: { 
+                    paddingTop: 10,
+                    backgroundColor: isDark ? colors.dark : 'white',
+                },
+                tabBarActiveTintColor: isDark ? colors.yellow : colors.dark,
+                tabBarInactiveTintColor: isDark ? '#d3dae2' : '#808e9b',
+                headerStyle: { backgroundColor: isDark ? colors.dark : 'white', },
+                headerTitleStyle: { color: isDark ? 'white' : colors.dark, },
             }}
         >
             <Tab.Screen name='Movies' component={Movies} />
