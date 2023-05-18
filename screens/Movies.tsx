0f173c6df231d9bfb81dcb812281863d470cc665
Son Haechang <sonhaechang@Sonui-MacBookPro.local>
2023-05-19 02:07:53 +0900

    refector: Movies Screen part One
    1. install react-native-web-swiper, react-native-dotenv, @types/react-native-dotenv
    2. swiper를 사용해서 Movies.tsx에 이미지 슬라이더 생성
    3. react-native-dotenv로 TMDB api key 환경 변수 설정
    4. fetch로 TMDB api 요청

--- screens/Movies.tsx
+++ screens/Movies.tsx
@@ -1,29 +1,49 @@
 import React from 'react';
+import { Dimensions } from 'react-native';
 
 import styled from 'styled-components/native';
 
+import Swiper from 'react-native-web-swiper';
+
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
 
+import { TMDB_API_KEY } from '@env';
 
-const Btn = styled.TouchableOpacity`
-    flex: 1; 
-    justify-content: center;
-    align-items: center;
-    background-color: ${props => props.theme.mainBgColor};
+
+const Container = styled.ScrollView`
+    background-color: ${(props) => props.theme.mainBgColor};
 `;
 
-const Title = styled.Text`
-    color: ${props => props.theme.textColor};
-`
+const View = styled.View`
+    flex: 1;
+`;
+
+const { height: SCREEN_HEIGHT } = Dimensions.get('window');
 
 type MoviesScreenProps = NativeStackScreenProps<any, 'Movies'>;
 
 export default function Movies({ navigation: { navigate }}: MoviesScreenProps) {
+    const getNowPlaying = () => {
+        const url = `https://api.themoviedb.org/3/movie/now_playing?api_key=${TMDB_API_KEY}&language=en-US&page=1&region=KR`;
+        fetch(url);
+    }
+
     return (
-        <Btn 
-            onPress={() => navigate('Stack', { screen: 'Three' })}
-        >
-            <Title>Movies</Title>
-        </Btn>
+        <Container>
+            <Swiper 
+                loop
+                timeout={3.5}
+                controlsEnabled={false}
+                containerStyle={{ 
+                    width: '100%', 
+                    height: SCREEN_HEIGHT / 4 
+                }}
+            >
+                <View style={{ backgroundColor: 'red' }}></View>
+                <View style={{ backgroundColor: 'blue' }}></View>
+                <View style={{ backgroundColor: 'red' }}></View>
+                <View style={{ backgroundColor: 'blue' }}></View>
+            </Swiper>
+        </Container>
     );
 }
